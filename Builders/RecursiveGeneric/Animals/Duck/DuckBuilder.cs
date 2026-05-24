522cbb40f52cad80761cbde65a4adaa28ee7aba8
Ibrahim Mohammad Ibrahim Abuhamdeh <RAA022@diyarme.com>
2026-05-24 17:12:57 +0300

    adding notes file to duck builder

--- Builders/RecursiveGeneric/Animals/Duck/DuckBuilder.cs
+++ Builders/RecursiveGeneric/Animals/Duck/DuckBuilder.cs
@@ -1,6 +1,6 @@
 namespace Builders.RecursiveGeneric.Animals.Duck;
 
-// can not create instance from DuckBuildAggregator<DuckBuilder> so DuckBuilder is necessary, look "Builder" at Duck.cs
+// can not create instance from DuckBuildAggregator<Self> so DuckBuilder is necessary, look "Builder" at Duck.cs
 public sealed class DuckBuilder : DuckBuildAggregator<DuckBuilder>
 {
     
